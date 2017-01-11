f6e5591a02392a48f7dc6e087eebbc6909272a13
pcbaldwin <peter@egloo.ca>
2017-01-11 09:48:27 -0500

    Moved post-install script

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'intrusion_prevention';
-$app['version'] = '2.2.3';
+$app['version'] = '2.3.0';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';

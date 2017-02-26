a46e2a43deabe9d01037298550156d6ebc4ffde0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:29:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'La Intrusion Prevention System aktive monitoras reto trafiko kaj blokoj nedeziratan trafiko antaŭ ĝi povas malutili via reto.';
+$lang['intrusion_prevention_app_name'] = 'Entrudiĝo Prevention System';
+$lang['intrusion_prevention_block_time'] = 'bloko Tempo';
+$lang['intrusion_prevention_blocked_list'] = 'Blokitaj Listo';
+$lang['intrusion_prevention_checksum_invalid'] = 'Checksum estas nevalida.';
+$lang['intrusion_prevention_security_id'] = 'sekureco ID';
+$lang['intrusion_prevention_white_list'] = 'blanka Listo';

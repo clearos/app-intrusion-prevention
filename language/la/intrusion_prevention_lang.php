9ab29592d850620bd6fcc3cd9cbe08fbf35c6196
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:33:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'Ratio monitors network traffic operantur intrusionem praeventionis et statuerunt ante traffic unwanted quod potest nocere vestri network.';
+$lang['intrusion_prevention_app_name'] = 'Ratio intrusionem praeventionis';
+$lang['intrusion_prevention_block_time'] = 'Block Tempus';
+$lang['intrusion_prevention_blocked_list'] = 'clausus List';
+$lang['intrusion_prevention_checksum_invalid'] = 'Checksum est invalidum.';
+$lang['intrusion_prevention_security_id'] = 'Id Security';
+$lang['intrusion_prevention_white_list'] = 'Album Album';

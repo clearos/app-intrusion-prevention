a94066c43104e7b184a0a2f51116ad9187099181
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:37:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'Системаи Пешгирии беиҷозат фаъолона назорат ҳаракати шабака ва блокҳои ҳаракати номатлуб пеш аз он метавонад шабакаи шумо зарар расонад.';
+$lang['intrusion_prevention_app_name'] = 'Системаи Пешгирии беиҷозат';
+$lang['intrusion_prevention_block_time'] = 'Time блоки';
+$lang['intrusion_prevention_blocked_list'] = 'Рӯйхати баста';
+$lang['intrusion_prevention_checksum_invalid'] = 'Checksum нодуруст аст.';
+$lang['intrusion_prevention_security_id'] = 'ID Амният';
+$lang['intrusion_prevention_white_list'] = 'Рӯйхати Сафед';

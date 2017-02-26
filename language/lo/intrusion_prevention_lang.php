0e092685b56109ff0abcf24ae15bd5794315a45f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:33:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'ລະບົບປ້ອງກັນການບຸກຮຸກຢ່າງຈິງຈັງຕິດຕາມກວດກາການຈະລາຈອນເຄືອຂ່າຍແລະຕັນການຈະລາຈອນທີ່ບໍ່ຕ້ອງການກ່ອນທີ່ມັນຈະສາມາດເປັນອັນຕະລາຍເຄືອຂ່າຍຂອງທ່ານ.';
+$lang['intrusion_prevention_app_name'] = 'ລະບົບປ້ອງກັນການບຸກລຸກ';
+$lang['intrusion_prevention_block_time'] = 'ທີ່ໃຊ້ເວລາ Block';
+$lang['intrusion_prevention_blocked_list'] = 'ຕ້ອງຫ້າມ';
+$lang['intrusion_prevention_checksum_invalid'] = 'ການກວດສອບແມ່ນບໍ່ຖືກຕ້ອງ.';
+$lang['intrusion_prevention_security_id'] = 'ID ຄວາມປອດໄພ';
+$lang['intrusion_prevention_white_list'] = 'ບັນຊີສີຂາວ';

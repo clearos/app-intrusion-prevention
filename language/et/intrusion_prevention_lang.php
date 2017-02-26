b24d4a3e37e0be4117552ac6f81fc721a6fbd52f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:30:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'Sissetungivastane Süsteem jälgib aktiivselt võrguliiklust ja plokid soovimatu liiklus enne, kui see võib kahjustada teie võrku.';
+$lang['intrusion_prevention_app_name'] = 'Sissetungi vältimise süsteemi';
+$lang['intrusion_prevention_block_time'] = 'Block aeg';
+$lang['intrusion_prevention_blocked_list'] = 'blokeeritud';
+$lang['intrusion_prevention_checksum_invalid'] = 'Kontrollsumma ei sobi.';
+$lang['intrusion_prevention_security_id'] = 'Turvalisus ID';
+$lang['intrusion_prevention_white_list'] = 'soovitusnimekiri';

fdf08cc06849e23e99759a2fb997b8bf389b881c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:33:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'Intrusion Prevention System aktyviai stebi tinklo srautą ir blokuoja nepageidaujamą judėjimą, kol ji gali pakenkti jūsų tinklą.';
+$lang['intrusion_prevention_app_name'] = 'Įsibrovimų prevencijos sistema';
+$lang['intrusion_prevention_block_time'] = 'Visas skrydžio laikas';
+$lang['intrusion_prevention_blocked_list'] = 'Užblokuoti sąrašas';
+$lang['intrusion_prevention_checksum_invalid'] = 'Kontrolinė negalioja.';
+$lang['intrusion_prevention_security_id'] = 'Saugumo ID';
+$lang['intrusion_prevention_white_list'] = 'Balta sąrašas';

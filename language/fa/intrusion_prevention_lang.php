af7d5b1bbdb1bbb379bb7ab86c4c9cc88e980475
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:35:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'سیستم پیشگیری از نفوذ فعالانه نظارت بر ترافیک شبکه و بلوک های ترافیک ناخواسته قبل از آن می توانید شبکه شما آسیب برساند.';
+$lang['intrusion_prevention_app_name'] = 'سیستم پیشگیری از نفوذ';
+$lang['intrusion_prevention_block_time'] = 'زمان بلوک';
+$lang['intrusion_prevention_blocked_list'] = 'فهرست مسدود شده';
+$lang['intrusion_prevention_checksum_invalid'] = 'بررسی نامعتبر است.';
+$lang['intrusion_prevention_security_id'] = 'ID امنیت';
+$lang['intrusion_prevention_white_list'] = 'لیست سفید';

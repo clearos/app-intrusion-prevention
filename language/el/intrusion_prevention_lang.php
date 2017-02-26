087e0a4c9438a5f5762ee44fd7c690b8bfaddbdb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:30:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'Το σύστημα πρόληψης εισβολής παρακολουθεί ενεργά την κυκλοφορία του δικτύου και μπλοκάρει τα ανεπιθύμητα κυκλοφορίας πριν από αυτό μπορεί να βλάψει το δίκτυό σας.';
+$lang['intrusion_prevention_app_name'] = 'Σύστημα Πρόληψης Intrusion';
+$lang['intrusion_prevention_block_time'] = 'Χρόνος λειτουργίας';
+$lang['intrusion_prevention_blocked_list'] = 'Αποκλεισμένοι Λίστα';
+$lang['intrusion_prevention_checksum_invalid'] = 'Checksum δεν είναι έγκυρο.';
+$lang['intrusion_prevention_security_id'] = 'αναγνωριστικό ασφαλείας';
+$lang['intrusion_prevention_white_list'] = 'Λευκή Λίστα';

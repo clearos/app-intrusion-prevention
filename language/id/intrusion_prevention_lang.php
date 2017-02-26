48706a29d3bd5ddabf22a3b0f9d3ba7fa7800191
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:31:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/id/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'The Intrusion Prevention System aktif memonitor lalu lintas jaringan dan blok lalu lintas yang tidak diinginkan sebelum dapat membahayakan jaringan Anda.';
+$lang['intrusion_prevention_app_name'] = 'Sistem Intrusion Prevention';
+$lang['intrusion_prevention_block_time'] = 'blok Waktu';
+$lang['intrusion_prevention_blocked_list'] = 'Daftar diblokir';
+$lang['intrusion_prevention_checksum_invalid'] = 'Checksum tidak valid.';
+$lang['intrusion_prevention_security_id'] = 'ID keamanan';
+$lang['intrusion_prevention_white_list'] = 'Daftar putih';

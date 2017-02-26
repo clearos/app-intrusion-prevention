3ed7998f885c4ca734fcab5042088dbdc3fc6656
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:32:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'ನಿಮ್ಮ ನೆಟ್ವರ್ಕ್ಗೆ ಹಾನಿಯುಂಟು ಮೊದಲು ದಾಳಿ ತಡೆ ವ್ಯವಸ್ಥೆ ಸಕ್ರಿಯವಾಗಿ ಜಾಲ ದಟ್ಟಣೆಯನ್ನು ಮತ್ತು ಬ್ಲಾಕ್ಗಳನ್ನು ಅನಗತ್ಯ ಸಂಚಾರ ಮೇಲ್ವಿಚಾರಣೆ.';
+$lang['intrusion_prevention_app_name'] = 'ಮಧ್ಯಪ್ರವೇಶವನ್ನು ತಡೆಗಟ್ಟುವಿಕೆ ವ್ಯವಸ್ಥೆ';
+$lang['intrusion_prevention_block_time'] = 'ಬ್ಲಾಕ್ ಟೈಮ್';
+$lang['intrusion_prevention_blocked_list'] = 'ನಿರ್ಬಂಧಿಸಿದ ಪಟ್ಟಿ';
+$lang['intrusion_prevention_checksum_invalid'] = 'ಚೆಕ್ಸಮ್ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['intrusion_prevention_security_id'] = 'ಭದ್ರತಾ ಐಡಿ';
+$lang['intrusion_prevention_white_list'] = 'ಬಿಳಿ ಪಟ್ಟಿ';

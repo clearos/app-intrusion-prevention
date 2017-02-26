fab4f256e1442a164d2919f65fcc89946545c74b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:36:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'System-hortagga yimaada The si firfircoon kormeera gaadiidka network iyo blocks gaadiidka aan loo baahnayn ka hor inta aan waxyeelayn kara network.';
+$lang['intrusion_prevention_app_name'] = 'System-hortagga kaga yimaada';
+$lang['intrusion_prevention_block_time'] = 'Block Time';
+$lang['intrusion_prevention_blocked_list'] = 'List Four';
+$lang['intrusion_prevention_checksum_invalid'] = 'Dalbatay waa baadil.';
+$lang['intrusion_prevention_security_id'] = 'ID Security';
+$lang['intrusion_prevention_white_list'] = 'List White';

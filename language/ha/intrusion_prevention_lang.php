0617f2b56a566843704fcdfc11451beb8bf77ef9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:31:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'The intrusion Rigakafin System rayayye sa ido na cibiyar sadarwa traffic da tubalan maras so traffic da zai iya cutar da cibiyar sadarwa.';
+$lang['intrusion_prevention_app_name'] = 'Intrusion Rigakafin System';
+$lang['intrusion_prevention_block_time'] = 'block Time';
+$lang['intrusion_prevention_blocked_list'] = 'An katange List';
+$lang['intrusion_prevention_checksum_invalid'] = 'Checksum ba daidai ba ne.';
+$lang['intrusion_prevention_security_id'] = 'Tsaro ID';
+$lang['intrusion_prevention_white_list'] = 'White List';

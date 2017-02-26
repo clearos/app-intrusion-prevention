9cddeffbe5bfd4139956d55df077001b05686904
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:39:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'די ינטרוזשאַן פּרעווענטיאָן סיסטעם אַקטיוולי מאָניטאָרס נעץ פאַרקער און בלאַקס אַנוואָנטיד פאַרקער איידער עס קענען שאַטן אייער נעץ.';
+$lang['intrusion_prevention_app_name'] = 'ינטרוסיאָן פּרעווענטיאָן סיסטעם';
+$lang['intrusion_prevention_block_time'] = 'בלאַק Time';
+$lang['intrusion_prevention_blocked_list'] = 'בלאַקט רשימה';
+$lang['intrusion_prevention_checksum_invalid'] = 'טשעקקסום איז פאַרקריפּלט.';
+$lang['intrusion_prevention_security_id'] = 'סעקוריטי שייַן';
+$lang['intrusion_prevention_white_list'] = 'ווייסע ליסטע';

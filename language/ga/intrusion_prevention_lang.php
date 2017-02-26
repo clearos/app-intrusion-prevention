831a92c01aba657ad928f54fcbe60d8e8da68072
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:31:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'An Córas a chosc cur isteach monatóireacht ghníomhach ar thrácht líonra agus bloic tráchta nach bhfuil ag teastáil sular féidir sé díobháil do líonra.';
+$lang['intrusion_prevention_app_name'] = 'Córas a chosc cur isteach';
+$lang['intrusion_prevention_block_time'] = 'Bloc Ama';
+$lang['intrusion_prevention_blocked_list'] = 'Liosta Bactha';
+$lang['intrusion_prevention_checksum_invalid'] = 'Is suim sheiceála neamhbhailí.';
+$lang['intrusion_prevention_security_id'] = 'ID Slándáil';
+$lang['intrusion_prevention_white_list'] = 'Liosta Bán';

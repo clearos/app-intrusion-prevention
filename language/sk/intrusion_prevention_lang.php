29dfe4bef38f760d4e64c649709fff9be48fe711
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:36:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sk/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'Systém IPS aktívne monitoruje sieťovú prevádzku a blokuje nežiaduce prevádzku predtým, než môže poškodiť vašu sieť.';
+$lang['intrusion_prevention_app_name'] = 'Intrusion Prevention System';
+$lang['intrusion_prevention_block_time'] = 'blok Time';
+$lang['intrusion_prevention_blocked_list'] = 'zoznam blokovaných';
+$lang['intrusion_prevention_checksum_invalid'] = 'Kontrolný súčet je neplatný.';
+$lang['intrusion_prevention_security_id'] = 'bezpečnostný kód';
+$lang['intrusion_prevention_white_list'] = 'biela listina';

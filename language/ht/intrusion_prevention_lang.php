57d284138a91e15db6e77dd66298aa18e9387019
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:31:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'Sistèm nan Prevansyon Entrizyon aktivman monitè rezo trafik ak blòk trafik vle anvan li kapab koze domaj rezo w lan.';
+$lang['intrusion_prevention_app_name'] = 'System prevansyon entrizyon';
+$lang['intrusion_prevention_block_time'] = 'Blòk Tan';
+$lang['intrusion_prevention_blocked_list'] = 'Blocked Lis';
+$lang['intrusion_prevention_checksum_invalid'] = 'Controls se valab.';
+$lang['intrusion_prevention_security_id'] = 'sekirite ID';
+$lang['intrusion_prevention_white_list'] = 'Blan Lis';

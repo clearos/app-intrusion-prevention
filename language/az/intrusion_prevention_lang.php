cd646d989d273fb61fff3ef20640a5438ccf3cef
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:28:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/az/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'Bu şəbəkə zərər əvvəl müdaxilənin qarşısının alınması sistemi aktiv şəbəkə trafik və blokları istenmeyen trafik nəzarət edir.';
+$lang['intrusion_prevention_app_name'] = 'Müdaxilənin qarşısının alınması sistemi';
+$lang['intrusion_prevention_block_time'] = 'Block Time';
+$lang['intrusion_prevention_blocked_list'] = 'Ban siyahısı';
+$lang['intrusion_prevention_checksum_invalid'] = 'Checksum yanlışdır.';
+$lang['intrusion_prevention_security_id'] = 'Security ID';
+$lang['intrusion_prevention_white_list'] = 'Ağ siyahısı';

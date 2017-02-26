412f32b8b5af451609a525a49e11d5bf1b5af748
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:38:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'Hệ thống ngăn chặn xâm nhập chủ động giám sát lưu lượng mạng và ngăn chặn giao thông không mong muốn trước khi nó có thể gây tổn hại cho mạng của bạn.';
+$lang['intrusion_prevention_app_name'] = 'Hệ thống ngăn chặn xâm nhập';
+$lang['intrusion_prevention_block_time'] = 'Khoảng thời gian';
+$lang['intrusion_prevention_blocked_list'] = 'Danh sách bị chặn';
+$lang['intrusion_prevention_checksum_invalid'] = 'Checksum là không hợp lệ.';
+$lang['intrusion_prevention_security_id'] = 'ID bảo mật';
+$lang['intrusion_prevention_white_list'] = 'Danh sách trắng';

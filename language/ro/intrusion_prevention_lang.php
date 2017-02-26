92d8f459c6cd40dfb99a63ab30c87370e9d5d8a6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:35:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'Sistemul de prevenire a intruziunilor monitorizează în mod activ traficul în rețea și blochează traficul nedorit înainte de a putea dăuna rețelei.';
+$lang['intrusion_prevention_app_name'] = 'Sistem de prevenire a accesului neautorizat';
+$lang['intrusion_prevention_block_time'] = 'bloc Timp';
+$lang['intrusion_prevention_blocked_list'] = 'Listă blocate';
+$lang['intrusion_prevention_checksum_invalid'] = 'Checksum este nevalid.';
+$lang['intrusion_prevention_security_id'] = 'ID-ul de securitate';
+$lang['intrusion_prevention_white_list'] = 'Lista albă';

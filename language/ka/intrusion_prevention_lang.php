43a88e10e2c26dcdffdd6799dbdb3c8ab330e9f2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:32:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'თავდასხმის პრევენციის სისტემის აქტიურად აკვირდება ქსელური ტრაფიკის და ბლოკავს არასასურველი ტრაფიკი სანამ ის შეიძლება ზიანი მიაყენოს თქვენს ქსელში.';
+$lang['intrusion_prevention_app_name'] = 'თავდასხმის პრევენციის სისტემა';
+$lang['intrusion_prevention_block_time'] = 'Block დრო';
+$lang['intrusion_prevention_blocked_list'] = 'დაბლოკილ სია';
+$lang['intrusion_prevention_checksum_invalid'] = 'ჯამი არასწორია.';
+$lang['intrusion_prevention_security_id'] = 'უშიშროების ID';
+$lang['intrusion_prevention_white_list'] = 'თეთრი სია';

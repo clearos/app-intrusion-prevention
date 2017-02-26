63c169b8caead5aad0d26f546be77f2dd357f922
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:29:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hr/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'Za sprječavanje sustav Protuprovalna aktivno prati mrežni promet i blokira neželjene prometne prije nego što može naškoditi svoju mrežu.';
+$lang['intrusion_prevention_app_name'] = 'Sustav za prevenciju upada';
+$lang['intrusion_prevention_block_time'] = 'Blok vrijeme';
+$lang['intrusion_prevention_blocked_list'] = 'Popis blokiranih';
+$lang['intrusion_prevention_checksum_invalid'] = 'Kontrolna je nevažeći.';
+$lang['intrusion_prevention_security_id'] = 'Sigurnosni ID';
+$lang['intrusion_prevention_white_list'] = 'Bijela lista';

a3dcc10740875c39b38c8c0b0b0828cdd4a7bcf5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:32:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = '이 네트워크에 손상을 줄 수 있습니다 전에 침입 방지 시스템이 적극적으로 네트워크 트래픽을 차단 원치 않는 트래픽을 모니터링합니다.';
+$lang['intrusion_prevention_app_name'] = '침입 방지 시스템';
+$lang['intrusion_prevention_block_time'] = '차단 시간';
+$lang['intrusion_prevention_blocked_list'] = '차단 목록';
+$lang['intrusion_prevention_checksum_invalid'] = '검사가 잘못되었습니다.';
+$lang['intrusion_prevention_security_id'] = '보안 ID';
+$lang['intrusion_prevention_white_list'] = '바람직한 것의 리스트';

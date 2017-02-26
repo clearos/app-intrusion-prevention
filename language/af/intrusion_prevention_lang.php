54dbddd62f8a5cb88cb6938ba8395f41a03e374a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:27:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/af/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'Die Intrusion Prevention System monitor aktief netwerk verkeer en blokke ongewenste verkeer voordat dit jou netwerk kan benadeel.';
+$lang['intrusion_prevention_app_name'] = 'Intrusion Prevention System';
+$lang['intrusion_prevention_block_time'] = 'Blok Tyd';
+$lang['intrusion_prevention_blocked_list'] = 'geblokkeer Lys';
+$lang['intrusion_prevention_checksum_invalid'] = 'Checksum is ongeldig.';
+$lang['intrusion_prevention_security_id'] = 'Security ID';
+$lang['intrusion_prevention_white_list'] = 'wit Lys';

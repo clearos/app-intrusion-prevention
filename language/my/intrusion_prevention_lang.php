fb273f321f48584e58422288d852d3370f5a4a74
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:29:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'ဒါကြောင့်သင့်ရဲ့ကွန်ယက်ကိုထိခိုက်စေနိုင်ပါတယ်ရှေ့တော်၌ထိုကျူးကျော်ဝင်ရောက်ခြင်းကိုကာကွယ်တားဆီးရေးစနစ်တက်ကြွစွာကွန်ယက်ကိုအသွားအလာနှင့်လုပ်ကွက်မလိုချင်တဲ့အသွားအလာစောင့်ကြည့်။';
+$lang['intrusion_prevention_app_name'] = 'ကျူးကျော်ဝင်ရောက်ခြင်းကိုကာကွယ်တားဆီးရေးစနစ်';
+$lang['intrusion_prevention_block_time'] = 'block အချိန်';
+$lang['intrusion_prevention_blocked_list'] = 'ပိတ်ဆို့စာရင်း';
+$lang['intrusion_prevention_checksum_invalid'] = 'checksum မမှန်ကန်ဘူး။';
+$lang['intrusion_prevention_security_id'] = 'လုံခြုံရေး ID ကို';
+$lang['intrusion_prevention_white_list'] = 'အဖြူရောင်စာရင်း';

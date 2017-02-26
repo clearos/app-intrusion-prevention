9114687b2453661fddd81a3edfe3585520f572b8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:35:54 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ru/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'Система предотвращения вторжений активно отслеживает сетевой трафик и блокирует нежелательный трафик, прежде чем он может нанести вред вашей сети.';
+$lang['intrusion_prevention_app_name'] = 'Система предотвращения вторжений';
+$lang['intrusion_prevention_block_time'] = 'Время блокировки';
+$lang['intrusion_prevention_blocked_list'] = 'Заблокированные Список';
+$lang['intrusion_prevention_checksum_invalid'] = 'Контрольная сумма является недействительным.';
+$lang['intrusion_prevention_security_id'] = 'Безопасность ID';
+$lang['intrusion_prevention_white_list'] = 'Белый список';

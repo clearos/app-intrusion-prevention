419681158c0365dd56f52ff93698c0dde514191e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:37:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'அது உங்கள் பிணைய தீங்கு விளைவிக்கலாம் முன் ஊடுருவல் தடுப்பு அமைப்பு தீவிரமாக நெட்வொர்க் போக்குவரத்து மற்றும் தொகுதிகள் தேவையற்ற போக்குவரத்து கண்காணிப்பு.';
+$lang['intrusion_prevention_app_name'] = 'ஊடுருவல் தடுப்பு அமைப்பு';
+$lang['intrusion_prevention_block_time'] = 'பிளாக் நேரம்';
+$lang['intrusion_prevention_blocked_list'] = 'தடுக்கப்பட்ட பட்டியல்';
+$lang['intrusion_prevention_checksum_invalid'] = 'செக்சம் தவறானது.';
+$lang['intrusion_prevention_security_id'] = 'பாதுகாப்பு ஐடி';
+$lang['intrusion_prevention_white_list'] = 'வெள்ளை பட்டியல்';

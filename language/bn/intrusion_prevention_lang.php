44d706f011f188b5e8c362a38e053de3a5dbdbef
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:28:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'আগে এটা আপনার নেটওয়ার্কের ক্ষতি করতে অনুপ্রবেশের প্রতিরোধ সিস্টেম সক্রিয় নেটওয়ার্ক ট্রাফিক এবং ব্লক অবাঞ্ছিত ট্রাফিক নিরীক্ষণ করা হয়.';
+$lang['intrusion_prevention_app_name'] = 'অনুপ্রবেশের প্রতিরোধ সিস্টেম';
+$lang['intrusion_prevention_block_time'] = 'ব্লক সময়';
+$lang['intrusion_prevention_blocked_list'] = 'অবরুদ্ধ তালিকা';
+$lang['intrusion_prevention_checksum_invalid'] = 'চেকসাম অবৈধ.';
+$lang['intrusion_prevention_security_id'] = 'সিকিউরিটি আইডি';
+$lang['intrusion_prevention_white_list'] = 'হোয়াইট তালিকা';

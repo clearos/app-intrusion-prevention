cf01f3715a57e2ca203c5ba2cbea0c1534fba898
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:32:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'ប្រព័ន្ធការពារការលុកលុយចរាចរបណ្តាញយ៉ាងសកម្មត្រួតពិនិត្យចរាចរណ៍ដែលមិនចង់បាននិងការប្លុកមុនពេលវាអាចប៉ះពាល់ដល់បណ្តាញរបស់អ្នក។';
+$lang['intrusion_prevention_app_name'] = 'ប្រព័ន្ធការពារការឈ្លានពាន';
+$lang['intrusion_prevention_block_time'] = 'ប្លុកម៉ោង';
+$lang['intrusion_prevention_blocked_list'] = 'បញ្ជីទប់ស្កាត់';
+$lang['intrusion_prevention_checksum_invalid'] = 'ឆេកសាំមិនត្រឹមត្រូវ។';
+$lang['intrusion_prevention_security_id'] = 'លេខសម្គាល់សន្ដិសុខ';
+$lang['intrusion_prevention_white_list'] = 'បញ្ជីស';

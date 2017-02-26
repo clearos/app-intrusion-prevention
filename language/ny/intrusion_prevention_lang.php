dc3ffbc724c5d01396ed9143785ab70156d09e67
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:29:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'The Zochitika Prevention System mwachangu oyang&#39;anira Intaneti magalimoto ndi midadada magalimoto zapathengo asanafike zingasokoneze maukonde anu.';
+$lang['intrusion_prevention_app_name'] = 'Zochitika System Prevention';
+$lang['intrusion_prevention_block_time'] = 'Block Time';
+$lang['intrusion_prevention_blocked_list'] = 'watsekedwa List';
+$lang['intrusion_prevention_checksum_invalid'] = 'Checksum ndi opanda pake.';
+$lang['intrusion_prevention_security_id'] = 'Security ID';
+$lang['intrusion_prevention_white_list'] = 'List White';

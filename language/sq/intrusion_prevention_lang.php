4b2175e68118844a266ff778b9709157077c07dc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:27:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'Sistemi Parandalimi Intrusion monitoron në mënyrë aktive e trafikut të rrjetit dhe blloqe trafikut padëshiruar para se ajo mund të dëmtojë rrjetin tuaj.';
+$lang['intrusion_prevention_app_name'] = 'Intrusion Prevention Sistemi';
+$lang['intrusion_prevention_block_time'] = 'Block Koha';
+$lang['intrusion_prevention_blocked_list'] = 'Lista e bllokuar';
+$lang['intrusion_prevention_checksum_invalid'] = 'Checksum është e pavlefshme.';
+$lang['intrusion_prevention_security_id'] = 'ID Security';
+$lang['intrusion_prevention_white_list'] = 'Lista e bardhë';

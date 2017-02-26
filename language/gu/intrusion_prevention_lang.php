c0e1fe5b8ba3538405acf1eca784cec95e0da148
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:30:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'તે પહેલાં તમારા નેટવર્ક નુકસાન પહોંચાડી શકે છે ઇન્ટ્રુઝન પ્રિવેન્શન સિસ્ટમ સક્રિય નેટવર્ક ટ્રાફિક અને બ્લોકો અનિચ્છનીય ટ્રાફિક મોનીટર કરે છે.';
+$lang['intrusion_prevention_app_name'] = 'ઇન્ટ્રુઝન પ્રિવેન્શન સિસ્ટમ';
+$lang['intrusion_prevention_block_time'] = 'બ્લોક સમય';
+$lang['intrusion_prevention_blocked_list'] = 'અવરુદ્ધ યાદી';
+$lang['intrusion_prevention_checksum_invalid'] = 'ચકાસણી અમાન્ય છે.';
+$lang['intrusion_prevention_security_id'] = 'સુરક્ષા ID ને';
+$lang['intrusion_prevention_white_list'] = 'વ્હાઇટ યાદી';

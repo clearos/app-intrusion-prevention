110d94aa4164b4762fb9f395c1c440f8f97fdf79
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:33:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mk/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'Систем за спречување упад активно го следи сообраќајот во мрежата и го блокира несаканите сообраќај пред тоа може да наштети на вашата мрежа.';
+$lang['intrusion_prevention_app_name'] = 'Упад превенција систем';
+$lang['intrusion_prevention_block_time'] = 'блок на времето';
+$lang['intrusion_prevention_blocked_list'] = 'Список на блокирани';
+$lang['intrusion_prevention_checksum_invalid'] = 'Проверката не е валиден.';
+$lang['intrusion_prevention_security_id'] = 'За безбедност на лични';
+$lang['intrusion_prevention_white_list'] = 'Белата листа';

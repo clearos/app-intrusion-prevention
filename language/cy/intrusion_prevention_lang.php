cfac8182facc2262855c8a7f88a7f1aba526fba5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:38:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'Mae&#39;r System Atal Ymyrraeth weithredol yn monitro traffig rhwydwaith a blociau traffig diangen cyn y gall niweidio eich rhwydwaith.';
+$lang['intrusion_prevention_app_name'] = 'System Atal Ymyrraeth';
+$lang['intrusion_prevention_block_time'] = 'bloc Amser';
+$lang['intrusion_prevention_blocked_list'] = 'Rhestr wedi&#39;u Rhwystro';
+$lang['intrusion_prevention_checksum_invalid'] = 'Checksum yn annilys.';
+$lang['intrusion_prevention_security_id'] = 'diogelwch ID';
+$lang['intrusion_prevention_white_list'] = 'Rhestr gwyn';

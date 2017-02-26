c2a7ecd69ebe3f16af08948d53b50a2ad8544eb1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:27:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'نظام منع الاختراق تراقب بنشاط حركة مرور الشبكة وكتل غير مرغوب فيه قبل أن تضر الشبكة.';
+$lang['intrusion_prevention_app_name'] = 'نظام منع الاختراق';
+$lang['intrusion_prevention_block_time'] = 'كتلة الوقت';
+$lang['intrusion_prevention_blocked_list'] = 'القائمة المحظورة';
+$lang['intrusion_prevention_checksum_invalid'] = 'اختباري غير صالح.';
+$lang['intrusion_prevention_security_id'] = 'معرف الأمان';
+$lang['intrusion_prevention_white_list'] = 'قائمة بيضاء';

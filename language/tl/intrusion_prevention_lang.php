d169ff7a6f6957c94879ee4bbe740cf9f432422d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:38:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'Ang panghihimasok Prevention System aktibong sinusubaybayan ang trapiko sa network at mga bloke hindi ginustong trapiko bago ito ay maaaring makapinsala sa iyong network.';
+$lang['intrusion_prevention_app_name'] = 'Panghihimasok Prevention System';
+$lang['intrusion_prevention_block_time'] = 'I-block ang Time';
+$lang['intrusion_prevention_blocked_list'] = 'Na-block List';
+$lang['intrusion_prevention_checksum_invalid'] = 'Checksum ay hindi wasto.';
+$lang['intrusion_prevention_security_id'] = 'Security ID';
+$lang['intrusion_prevention_white_list'] = 'White List';

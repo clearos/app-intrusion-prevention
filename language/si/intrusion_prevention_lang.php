0828e3963362a551e77e20e891dd71a468d40be4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:36:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'එය ඔබගේ ජාලයට හානි කිරීමට පෙර, අත පෙවීම වැළැක්වීමේ පද්ධතිය සක්රීයව ජාල හා වාරණ අනවශ්ය රථවාහන අධීක්ෂණය කරයි.';
+$lang['intrusion_prevention_app_name'] = 'අත පෙවීම වැළැක්වීමේ පද්ධතිය';
+$lang['intrusion_prevention_block_time'] = 'වාරණ කාල';
+$lang['intrusion_prevention_blocked_list'] = 'අවහිර ලැයිස්තුව';
+$lang['intrusion_prevention_checksum_invalid'] = 'තිර අගය වලංගු නොවේ.';
+$lang['intrusion_prevention_security_id'] = 'ආරක්ෂක හැඳුනුම්';
+$lang['intrusion_prevention_white_list'] = 'සුදු ලැයිස්තු';

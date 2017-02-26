87bf40328ffc063d8424153c7cd8f74a8ba76564
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:38:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'tarmoq zarar oldin Intrusion Prevention System faol tarmoq trafikni va bloklari istalmagan transport nazorat qiladi.';
+$lang['intrusion_prevention_app_name'] = 'Hujum oldini olish tizimi';
+$lang['intrusion_prevention_block_time'] = 'blok Time';
+$lang['intrusion_prevention_blocked_list'] = 'bloklangan ro&#39;yxati';
+$lang['intrusion_prevention_checksum_invalid'] = 'Summasi haqiqiy emas.';
+$lang['intrusion_prevention_security_id'] = 'Xavfsizlik ID';
+$lang['intrusion_prevention_white_list'] = 'oq ro&#39;yxati';

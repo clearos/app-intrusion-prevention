4d4a4cd5dc276b2a5dc114ffc6eb11391024a7aa
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:36:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'The Intrusion Thibelo System mafolofolo nna e beile leitlho ya netweke sephethephethe le masakana sephethephethe sa batleheng pele e ka lematsa netweke ya hao.';
+$lang['intrusion_prevention_app_name'] = 'Intrusion System Thibelo';
+$lang['intrusion_prevention_block_time'] = 'Block Time';
+$lang['intrusion_prevention_blocked_list'] = 'koetsoe List';
+$lang['intrusion_prevention_checksum_invalid'] = 'Checksum ha o sebetse.';
+$lang['intrusion_prevention_security_id'] = 'Security ID';
+$lang['intrusion_prevention_white_list'] = 'List White';

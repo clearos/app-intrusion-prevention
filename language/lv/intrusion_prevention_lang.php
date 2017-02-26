4ced7af3f8455b2c3042e586dbbb18a21c2cfa77
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:33:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'Ielaušanās novēršanas sistēma aktīvi uzrauga tīkla satiksmes un bloķē nevēlamu satiksmes, pirms tas var kaitēt jūsu tīklu.';
+$lang['intrusion_prevention_app_name'] = 'Ielaušanās novēršanas sistēma';
+$lang['intrusion_prevention_block_time'] = 'Block laiks';
+$lang['intrusion_prevention_blocked_list'] = 'bloķēts saraksts';
+$lang['intrusion_prevention_checksum_invalid'] = 'Kontrolsumma ir nederīga.';
+$lang['intrusion_prevention_security_id'] = 'drošības ID';
+$lang['intrusion_prevention_white_list'] = 'White saraksts';

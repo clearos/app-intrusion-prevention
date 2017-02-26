1e77b1e917431cd9144fbbf1e33f01c34b119abb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:34:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'Халдлага урьдчилан сэргийлэх систем идэвхтэй таны сүлжээнд хор хөнөөл учруулж өмнө сүлжээний урсгалыг болон блок Болохгүй урсгалыг хянаж байдаг.';
+$lang['intrusion_prevention_app_name'] = 'Халдлага урьдчилан сэргийлэх систем';
+$lang['intrusion_prevention_block_time'] = 'блок цаг';
+$lang['intrusion_prevention_blocked_list'] = 'хаагдсан жагсаалт';
+$lang['intrusion_prevention_checksum_invalid'] = 'Шалгалтын хүчин төгөлдөр бус байна.';
+$lang['intrusion_prevention_security_id'] = 'Аюулгүй байдлын ID';
+$lang['intrusion_prevention_white_list'] = 'Цагаан жагсаалт';

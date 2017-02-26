ddb55435a93354a4118fb5b2ad092d71fc245eb6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:39:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'The Ifọle Idena System actively diigi nẹtiwọki ijabọ ati awọn ohun amorindun ti aifẹ ijabọ ṣaaju ki o le še ipalara nẹtiwọki rẹ.';
+$lang['intrusion_prevention_app_name'] = 'Ifọle Idena System';
+$lang['intrusion_prevention_block_time'] = 'block Time';
+$lang['intrusion_prevention_blocked_list'] = 'dina Akojọ';
+$lang['intrusion_prevention_checksum_invalid'] = 'Checksum ni invalid.';
+$lang['intrusion_prevention_security_id'] = 'Security ID';
+$lang['intrusion_prevention_white_list'] = 'White Akojọ';

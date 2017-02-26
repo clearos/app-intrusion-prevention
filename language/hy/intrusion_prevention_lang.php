9f3c8260092e6640848bdb454b0c22dd05ad18e2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:28:05 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'The Ներխուժման կանխարգելման համակարգ ակտիվորեն մոնիտորներ ցանցի տրաֆիկի եւ բլոկների անցանկալի երթեւեկության նախքան այն կարող է վնասել ձեր ցանցը.';
+$lang['intrusion_prevention_app_name'] = 'Ներխուժման կանխարգելման համակարգ';
+$lang['intrusion_prevention_block_time'] = 'Արգելափակել Time';
+$lang['intrusion_prevention_blocked_list'] = 'Արգելափակված ցուցակ';
+$lang['intrusion_prevention_checksum_invalid'] = 'Checksum է անվավեր:';
+$lang['intrusion_prevention_security_id'] = 'Անվտանգության ID';
+$lang['intrusion_prevention_white_list'] = 'Սպիտակ ցուցակ';

deba688ff4c916491713996464546e71fda63d99
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:32:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'ол сіздің желі зиян келтіруі мүмкін, бұрын басып кіруді болдырмау жүйесі белсенді желілік трафик және блоктар қажетсіз трафикті бақылайды.';
+$lang['intrusion_prevention_app_name'] = 'Басып кіруді болдырмау жүйесі';
+$lang['intrusion_prevention_block_time'] = 'Блок Уақыт';
+$lang['intrusion_prevention_blocked_list'] = 'Құрсауланған тізімі';
+$lang['intrusion_prevention_checksum_invalid'] = 'Бақылау жарамсыз болып табылады.';
+$lang['intrusion_prevention_security_id'] = 'Қауіпсіздік ID';
+$lang['intrusion_prevention_white_list'] = 'Ақ тізім';

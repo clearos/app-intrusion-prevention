f6de5dc0cef51e7db26eac01c401e14d5fee84fb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:34:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'The Pencerobohan Sistem Pencegahan memantau secara aktif trafik rangkaian dan blok trafik yang tidak diingini sebelum ia boleh membahayakan rangkaian anda.';
+$lang['intrusion_prevention_app_name'] = 'Sistem Pencegahan Pencerobohan';
+$lang['intrusion_prevention_block_time'] = 'Block Masa';
+$lang['intrusion_prevention_blocked_list'] = 'Senarai disekat';
+$lang['intrusion_prevention_checksum_invalid'] = 'Hasil tambah semak adalah tidak sah.';
+$lang['intrusion_prevention_security_id'] = 'ID Keselamatan';
+$lang['intrusion_prevention_white_list'] = 'putih senarai';

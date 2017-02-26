736068a421982d62e5a2656bfd8cb96183230d3e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:29:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ca/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'El sistema de prevenció d&#39;intrusions supervisa activament el tràfic de xarxa i bloqueja el trànsit no desitjat abans que pugui fer mal a la seva xarxa.';
+$lang['intrusion_prevention_app_name'] = 'Sistema de prevenció d&#39;intrusions';
+$lang['intrusion_prevention_block_time'] = 'Temps de bloqueig';
+$lang['intrusion_prevention_blocked_list'] = 'Llista de bloquejats';
+$lang['intrusion_prevention_checksum_invalid'] = 'Suma de comprovació no és vàlida.';
+$lang['intrusion_prevention_security_id'] = 'Identificació de seguretat';
+$lang['intrusion_prevention_white_list'] = 'llista blanca';

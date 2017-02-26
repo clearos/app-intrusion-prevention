bff452bcea44848a88818a86491276f42d85f9fa
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:35:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'ਅੱਗੇ ਇਸ ਨੂੰ ਆਪਣੇ ਨੈੱਟਵਰਕ ਨੂੰ ਨੁਕਸਾਨ ਹੋ ਸਕਦਾ ਹੈ ਘੁਸਪੈਠ ਰੋਕੂ ਸਿਸਟਮ ਸਰਗਰਮੀ ਨੈੱਟਵਰਕ ਟਰੈਫਿਕ ਅਤੇ ਬਲਾਕ ਅਣਚਾਹੇ ਆਵਾਜਾਈ ਦੀ ਨਿਗਰਾਨੀ.';
+$lang['intrusion_prevention_app_name'] = 'ਘੁਸਪੈਠ ਦੀ ਰੋਕਥਾਮ ਸਿਸਟਮ';
+$lang['intrusion_prevention_block_time'] = 'ਬਲਾਕ ਟਾਈਮ';
+$lang['intrusion_prevention_blocked_list'] = 'ਰੋਕੀ ਸੂਚੀ';
+$lang['intrusion_prevention_checksum_invalid'] = 'ਚੈੱਕਸਮ ਗਲਤ ਹੈ.';
+$lang['intrusion_prevention_security_id'] = 'ਸੁਰੱਖਿਆ ਦਾ ID';
+$lang['intrusion_prevention_white_list'] = 'ਵ੍ਹਾਈਟ ਸੂਚੀ';

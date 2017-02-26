97c5d2c9e8239343012fdfac81a6987047d93c40
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:37:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'Kuingilia Kuzuia System kikamilifu wachunguzi mtandao wa trafiki na vitalu trafiki zisizohitajika kabla inaweza kuwadhuru mtandao wako.';
+$lang['intrusion_prevention_app_name'] = 'Intrusion Kuzuia System';
+$lang['intrusion_prevention_block_time'] = 'Block Muda';
+$lang['intrusion_prevention_blocked_list'] = 'imefungwa Orodha';
+$lang['intrusion_prevention_checksum_invalid'] = 'Checksum ni batili.';
+$lang['intrusion_prevention_security_id'] = 'Security ID';
+$lang['intrusion_prevention_white_list'] = 'Orodha White';

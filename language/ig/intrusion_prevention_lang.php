e5f7e2dafc708e718e6d04b14dea86272dbcb61c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:31:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'The intrusion Prevention System ifịk ifịk Monitors netwọk okporo ụzọ na blocks-achọghị okporo ụzọ n&#39;ihu ya nwere ike imerụ gị na netwọk.';
+$lang['intrusion_prevention_app_name'] = 'Intrusion Prevention System';
+$lang['intrusion_prevention_block_time'] = 'ngọngọ Time';
+$lang['intrusion_prevention_blocked_list'] = 'Egbochiri List';
+$lang['intrusion_prevention_checksum_invalid'] = 'Checksum bụ ghara ịdị irè.';
+$lang['intrusion_prevention_security_id'] = 'Security ID';
+$lang['intrusion_prevention_white_list'] = 'White List';

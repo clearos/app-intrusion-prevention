f1ea426ed2c477fed393cba788f544ec8063b95d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:37:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/th/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'ระบบป้องกันการบุกรุกจะตรวจสอบเครือข่ายการจราจรและบล็อกการจราจรที่ไม่พึงประสงค์ก่อนที่มันจะสามารถเป็นอันตรายต่อเครือข่ายของคุณ';
+$lang['intrusion_prevention_app_name'] = 'ระบบป้องกันการบุกรุก';
+$lang['intrusion_prevention_block_time'] = 'เวลาที่ถูกบล็อก';
+$lang['intrusion_prevention_blocked_list'] = 'รายการที่ถูกบล็อก';
+$lang['intrusion_prevention_checksum_invalid'] = 'การตรวจสอบไม่ถูกต้อง';
+$lang['intrusion_prevention_security_id'] = 'รหัสรักษาความปลอดภัย';
+$lang['intrusion_prevention_white_list'] = 'รายการสีขาว';

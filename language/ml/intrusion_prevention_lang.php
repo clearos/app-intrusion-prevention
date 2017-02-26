43af1377830665b441158cb4e075d0a07c7c5266
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:34:05 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'അതു നിങ്ങളുടെ നെറ്റ്വർക്ക് ദോഷകരമാകും മുമ്പ് Intrusion പ്രിവൻഷൻ സിസ്റ്റം സജീവമായി നെറ്റ്വർക്ക് ട്രാഫിക് ബ്ലോക്കുകൾ അനാവശ്യ ട്രാഫിക് നിരീക്ഷിക്കുന്നു.';
+$lang['intrusion_prevention_app_name'] = 'Intrusion പ്രിവൻഷൻ സിസ്റ്റം';
+$lang['intrusion_prevention_block_time'] = 'ബ്ലോക്ക് സമയം';
+$lang['intrusion_prevention_blocked_list'] = 'തടഞ്ഞ പട്ടികയിൽ';
+$lang['intrusion_prevention_checksum_invalid'] = 'ചെക്ക്സം അസാധുവാണ്.';
+$lang['intrusion_prevention_security_id'] = 'സുരക്ഷാ ഐഡി';
+$lang['intrusion_prevention_white_list'] = 'വൈറ്റ് ലിസ്റ്റ്';

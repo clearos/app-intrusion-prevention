3b13ad83a54ec42c3944a77371f89e8b58f195bd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:38:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ur/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'یہ آپ کے نیٹ ورک کو نقصان پہنچانے سے قبل مداخلت کی روک تھام کے نظام کو فعال طور پر نیٹ ورک ٹریفک اور بلاکس ناپسندیدہ ٹریفک پر نظر رکھتا ہے.';
+$lang['intrusion_prevention_app_name'] = 'مداخلت کی روک تھام کے نظام';
+$lang['intrusion_prevention_block_time'] = 'بلاک وقت';
+$lang['intrusion_prevention_blocked_list'] = 'بلاک لسٹ';
+$lang['intrusion_prevention_checksum_invalid'] = 'حیض غلط ہے.';
+$lang['intrusion_prevention_security_id'] = 'سیکورٹی ID';
+$lang['intrusion_prevention_white_list'] = 'وائٹ لسٹ';

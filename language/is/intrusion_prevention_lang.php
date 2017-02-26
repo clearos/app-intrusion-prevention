6d6777bb6b7300d562b0bfdbb3825e5423392226
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:32:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'The Átroðningur Forvarnir System fylgist virkan net umferð og blokkir óæskileg umferð áður en það getur skaðað net.';
+$lang['intrusion_prevention_app_name'] = 'Átroðningur Forvarnir System';
+$lang['intrusion_prevention_block_time'] = 'Block Time';
+$lang['intrusion_prevention_blocked_list'] = 'Bannaður List';
+$lang['intrusion_prevention_checksum_invalid'] = 'Stöðva er ógilt.';
+$lang['intrusion_prevention_security_id'] = 'Öryggi ID';
+$lang['intrusion_prevention_white_list'] = 'White List';

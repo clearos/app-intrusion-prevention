8fc353568b5695bdb9ee0a5947c6bbdda358d445
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:33:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'Fisorohana ny fitsabahana System tambajotra mavitrika-maso ny fifamoivoizana sy ny manakana ny fifamoivoizana teo anoloany izy tsy ilaina mety hanimba ny tambajotra.';
+$lang['intrusion_prevention_app_name'] = 'Fitsabahana Fisorohana System';
+$lang['intrusion_prevention_block_time'] = 'Block Time';
+$lang['intrusion_prevention_blocked_list'] = 'voasakana List';
+$lang['intrusion_prevention_checksum_invalid'] = 'Checksum dia tsy manan-kery.';
+$lang['intrusion_prevention_security_id'] = 'Security ID';
+$lang['intrusion_prevention_white_list'] = 'White List';

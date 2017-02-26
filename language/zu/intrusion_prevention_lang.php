4ea6557cdea102e36d06b3e3d8d2c689f07d9fb4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:39:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'Ukungena Prevention System ngenkuthalo uqaphe kwenethiwekhi emabhuloki traffic engadingeki ngaphambi ingakulimaza inethiwekhi yakho.';
+$lang['intrusion_prevention_app_name'] = 'Nsada Prevention System';
+$lang['intrusion_prevention_block_time'] = 'Block Isikhathi';
+$lang['intrusion_prevention_blocked_list'] = 'uHla Lwesizinda';
+$lang['intrusion_prevention_checksum_invalid'] = 'Checksum ayivumelekile.';
+$lang['intrusion_prevention_security_id'] = 'ID Security';
+$lang['intrusion_prevention_white_list'] = 'Uhlu White';

9ed15924a5a33c51ace2321584050741f12e0c6a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:37:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sv/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'Intrusion Prevention System övervakar aktivt nätverkstrafiken och blockerar oönskad trafik innan det kan skada ditt nätverk.';
+$lang['intrusion_prevention_app_name'] = 'Intrusion Prevention System';
+$lang['intrusion_prevention_block_time'] = 'Block Time';
+$lang['intrusion_prevention_blocked_list'] = 'Blockerade';
+$lang['intrusion_prevention_checksum_invalid'] = 'Kontrollsumma är ogiltig.';
+$lang['intrusion_prevention_security_id'] = 'säkerhets-ID';
+$lang['intrusion_prevention_white_list'] = 'vita listan';

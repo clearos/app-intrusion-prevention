76951b8e54078b1e4bd2c3c391360a053810c28b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:34:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ne/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'यसलाई आफ्नो नेटवर्क हानि गर्न सक्छ अघि घुसपैठ रोकथाम प्रणाली सक्रिय नेटवर्क ट्राफिक र ब्लक अनावश्यक ट्राफिक निगरानी।';
+$lang['intrusion_prevention_app_name'] = 'घुसपैठ रोकथाम प्रणाली';
+$lang['intrusion_prevention_block_time'] = 'ब्लक समय';
+$lang['intrusion_prevention_blocked_list'] = 'अवरुद्ध सूची';
+$lang['intrusion_prevention_checksum_invalid'] = 'चेकसम अमान्य छ।';
+$lang['intrusion_prevention_security_id'] = 'सुरक्षा आईडी';
+$lang['intrusion_prevention_white_list'] = 'सेतो सूची';

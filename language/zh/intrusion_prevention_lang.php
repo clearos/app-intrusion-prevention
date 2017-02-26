1fbbd6fb27d011f0e3162ba19da04ad9039e3e85
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:29:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = '入侵防护系统主动监控网络流量并阻止恶意流量，才可以伤害你的网络。';
+$lang['intrusion_prevention_app_name'] = '入侵防御系统';
+$lang['intrusion_prevention_block_time'] = '块时间';
+$lang['intrusion_prevention_blocked_list'] = '阻止列表';
+$lang['intrusion_prevention_checksum_invalid'] = '校验和是无效的。';
+$lang['intrusion_prevention_security_id'] = '安全ID';
+$lang['intrusion_prevention_white_list'] = '白名单';

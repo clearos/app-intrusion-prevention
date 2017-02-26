4ca0e3a2ac11138983bd26e0a5218d73485b51d3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:33:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'kaha aroturuki te Ārai Pūnaha Takahi hokohoko whatunga me poraka waka e hiahiatia te reira i mua i taea kino tō whatunga.';
+$lang['intrusion_prevention_app_name'] = 'Pokanoa Pūnaha Prevention';
+$lang['intrusion_prevention_block_time'] = 'Wā Poraka';
+$lang['intrusion_prevention_blocked_list'] = 'Rārangi āraia';
+$lang['intrusion_prevention_checksum_invalid'] = 'He muhu Checksum.';
+$lang['intrusion_prevention_security_id'] = 'ID Haumarutanga';
+$lang['intrusion_prevention_white_list'] = 'Rārangi White';

ca5dfc5770f1d7fc8e472a6434fe7cd4e5ffa96b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:37:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'మీ నెట్వర్క్కు హాని ముందు చొరబాటు నిరోధక వ్యవస్థ చురుగ్గా నెట్వర్క్ ట్రాఫిక్ మరియు బ్లాక్లను అవాంఛిత ట్రాఫిక్ పర్యవేక్షిస్తుంది.';
+$lang['intrusion_prevention_app_name'] = 'చొరబాటు నిరోధక వ్యవస్థ';
+$lang['intrusion_prevention_block_time'] = 'బ్లాక్ సమయం';
+$lang['intrusion_prevention_blocked_list'] = 'నిరోధిత జాబితా';
+$lang['intrusion_prevention_checksum_invalid'] = 'చెక్సమ్ చెల్లదు.';
+$lang['intrusion_prevention_security_id'] = 'భద్రతా ID';
+$lang['intrusion_prevention_white_list'] = 'వైట్ లిస్ట్';

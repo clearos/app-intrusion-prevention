7d78e9f5c90d67289daca8fd58c289c06fe43538
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:28:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bs/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'Sistem za sprečavanje upada aktivno prati mrežni promet i blokira neželjene saobraćaj prije nego što to može naškoditi svoju mrežu.';
+$lang['intrusion_prevention_app_name'] = 'Sistem za sprečavanje upada';
+$lang['intrusion_prevention_block_time'] = 'Block Time';
+$lang['intrusion_prevention_blocked_list'] = 'blokirani List';
+$lang['intrusion_prevention_checksum_invalid'] = 'Checksum je nevažeći.';
+$lang['intrusion_prevention_security_id'] = 'sigurnost ID';
+$lang['intrusion_prevention_white_list'] = 'Bijela lista';

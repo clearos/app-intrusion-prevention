ef9e7e77325f96b7ca479ddbeee26ada34489ee9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:31:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'Az Intrusion Prevention System aktívan figyeli a hálózati forgalmat, és blokkolja a nem kívánt forgalmat, mielőtt kárt okozhatnának a hálózatban.';
+$lang['intrusion_prevention_app_name'] = 'Intrusion Prevention System';
+$lang['intrusion_prevention_block_time'] = 'repülési idő';
+$lang['intrusion_prevention_blocked_list'] = 'blokkolt lista';
+$lang['intrusion_prevention_checksum_invalid'] = 'Checksum érvénytelen.';
+$lang['intrusion_prevention_security_id'] = 'biztonsági azonosító';
+$lang['intrusion_prevention_white_list'] = 'fehér lista';

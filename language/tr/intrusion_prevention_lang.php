4bb5ee03b8c22938485f7ad20eef56cc37f7b6aa
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 12:38:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/intrusion_prevention_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['intrusion_prevention_app_description'] = 'ağınıza zarar verebilecek önce Saldırı Önleme Sistemi aktif ağ trafiğini ve bloklar istenmeyen trafik izler.';
+$lang['intrusion_prevention_app_name'] = 'Saldırı Önleme Sistemi';
+$lang['intrusion_prevention_block_time'] = 'blok Zaman';
+$lang['intrusion_prevention_blocked_list'] = 'engellenen Liste';
+$lang['intrusion_prevention_checksum_invalid'] = 'Checksum geçersiz.';
+$lang['intrusion_prevention_security_id'] = 'Güvenlik kimliği';
+$lang['intrusion_prevention_white_list'] = 'beyaz Liste';
